3ac4b44f076b425e797fe2ea356dc3855371b9ea
David okoronkwo <77294470+david4972@users.noreply.github.com>
2022-07-05 00:31:27 -0400

    Update DigBankSample.java

--- DigBankSample.java
+++ DigBankSample.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class BankSample {
 
 
+
     public static void intro() throws NoSuchAlgorithmException, SQLException, IOException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please select what you'd like to do today?");
@@ -119,7 +120,7 @@ public class BankSample {
         Inov.create_credit_account(account_name, account_email, account_address);
     }
 
-    public static void send_money() throws SQLException {
+    public static void send_money() throws SQLException, IOException {
         inov Inov = new inov();
         Scanner scan = new Scanner(System.in);
         Scanner scan2 = new Scanner(System.in);
@@ -130,7 +131,7 @@ public class BankSample {
         System.out.print("2. No ");
         String inter_choice = scan4.nextLine();
         if (inter_choice.equals("1")) {
-            send_money_internationally();
+            international_debit_send_money();
         } else {
             System.out.print("Please enter virtual account number: ");
             int account_num = scan.nextInt();
@@ -151,79 +152,39 @@ public class BankSample {
         }
     }
 
-    public static void send_money_internationally() throws SQLException {
+    public static void international_debit_send_money() throws SQLException, IOException {
          inov Inov2 = new inov();
          Scanner scan_interdebit = new Scanner(System.in);
          Scanner scan_interdebit2 = new Scanner(System.in);
          Scanner scan_interdebit3 = new Scanner(System.in);
+        Scanner scan_interdebit4 = new Scanner(System.in);
+        Scanner scan_interdebit5 = new Scanner(System.in);
          System.out.print("Please enter virtual account number: ");
          int account_num = scan_interdebit.nextInt();
          System.out.print("Please enter name of person receiving money: ");
          String receive = scan_interdebit2.nextLine();
          System.out.print("Please enter amount you want to transfer: ");
-         double account_deposit = scan_interdebit3.nextDouble();
-         Inov2.send_money_debit_international(account_num, account_deposit, receive);
+         double account_val = scan_interdebit3.nextDouble();
+        System.out.print("Please enter Country code of Country you are located Ex USD (United States): ");
+        String account_base_currency = scan_interdebit4.nextLine();
+        System.out.print("Please enter name of Country where person receiving money is located: ");
+        String account_convert_currency = scan_interdebit5.nextLine();
+        Inov2.International_debit_send(account_base_currency, account_convert_currency, account_num, account_val, receive);
     }
 
     public static void Currency_exchange() throws SQLException, IOException {
         inov Inov2 = new inov();
         Scanner scan_interdebit = new Scanner(System.in);
-        // Scanner scan_interdebit2 = new Scanner(System.in);
+        Scanner scan_interdebit2 = new Scanner(System.in);
         Scanner scan_interdebit3 = new Scanner(System.in);
-        Scanner scan_interdebit4 = new Scanner(System.in);
+        //Scanner scan_interdebit4 = new Scanner(System.in);
         System.out.print("Please enter virtual account number: ");
         int account_num = scan_interdebit.nextInt();
-        System.out.println("Please select ?");
-        System.out.println("1. Debit ");
-        System.out.println("2. Credit ");
-        // System.out.println("3. International Debit ");
-        String accnt_line = scan_interdebit3.nextLine();
-        System.out.println("Please select ?");
-        System.out.println("1. Euros ");
-        System.out.println("2. British Pounds ");
-        System.out.println("3. Australian Dollars ");
-        System.out.println("4. Japanese Yen ");
-        System.out.println("5. Hong Kong Dollars ");
-        String curr_convert = scan_interdebit4.nextLine();
-        if (accnt_line.equals("1"))
-            if (curr_convert.equals("1")){
-                Inov2.Currency_exchange_Debit_EUR(account_num);
-        } if (accnt_line.equals("1"))
-            if (curr_convert.equals("2")){
-                Inov2.Currency_exchange_Debit_GBP(account_num);
-            }
-        if (accnt_line.equals("1"))
-            if (curr_convert.equals("3")){
-                Inov2.Currency_exchange_Debit_AUS(account_num);
-            }
-        if (accnt_line.equals("1"))
-            if (curr_convert.equals("4")){
-                Inov2.Currency_exchange_Debit_JPY(account_num);
-            }
-        if (accnt_line.equals("1"))
-            if (curr_convert.equals("5")){
-                Inov2.Currency_exchange_Debit_HKD(account_num);
-            }
-        if (accnt_line.equals("2"))
-            if (curr_convert.equals("1")){
-                Inov2.Currency_exchange_Credit_EUR(account_num);
-            } if (accnt_line.equals("2"))
-            if (curr_convert.equals("2")){
-                Inov2.Currency_exchange_Credit_GBP(account_num);
-            }
-        if (accnt_line.equals("2"))
-            if (curr_convert.equals("3")){
-                Inov2.Currency_exchange_Credit_AUS(account_num);
-            }
-        if (accnt_line.equals("2"))
-            if (curr_convert.equals("4")){
-                Inov2.Currency_exchange_Credit_JPY(account_num);
-            }
-        if (accnt_line.equals("2"))
-            if (curr_convert.equals("5")){
-                Inov2.Currency_exchange_Credit_HongKong(account_num);
-            }
-
+        System.out.print("Please enter Country code of Country you are located Ex USD (United States): ");
+        String account_Currency_base = scan_interdebit2.nextLine();
+        System.out.print("Please enter name of Country you which to convert currency: ");
+        String account_convert_currency = scan_interdebit3.nextLine();
+        Inov2.Currency_exchange(account_Currency_base, account_convert_currency, account_num);
 
     }
 
